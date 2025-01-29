d07ebdcfe5bb2ab0d2e7912d64c225b56c821caf
Marko Knepper <m.knepper@ub.uni-mainz.de>
2025-01-29 16:22:18 +0100

    New version 5

--- signaturenDruck/main.js
+++ signaturenDruck/main.js
@@ -322,7 +322,7 @@ function createWindow () {
     Menu.setApplicationMenu(menu)
   }
   // set the mainwindow title (name + version from package.json)
-  mainWindow.setTitle('Signaturendruck v' + app.getVersion() + ' Mz v4')
+  mainWindow.setTitle('Signaturendruck v' + app.getVersion() + ' Mz v5')
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, '/html/index.html'),
