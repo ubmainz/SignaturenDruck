e6ecb2c6dd834b51c59edb34ad0542dc897a3b53
Marko Knepper <m.knepper@ub.uni-mainz.de>
2025-03-11 18:50:51 +0100

    NC files

--- signaturenDruck/main.js
+++ signaturenDruck/main.js
@@ -370,9 +370,9 @@ function checkConfig () {
     createConfig()
   }
   if (config.get('mode.defaultMode') === 'TSKModus') {
-    createModeFiles('TSKModus', ['RVK', 'TSK_Magazin_Zs', 'TSK_Magazin_Mo', 'TSK_Lehrbuchsammlung', 'TSK_Freihand', 'TSK_Lesesaal'])
+    createModeFiles('TSKModus', ['RVK', 'NC', 'TSK_Magazin_Zs', 'TSK_Magazin_Mo', 'TSK_Lehrbuchsammlung', 'TSK_Freihand', 'TSK_Lesesaal'])
   } else if (config.get('mode.defaultMode') === 'defaultMode') {
-    createModeFiles('defaultMode', ['RVK', 'ZB_Magazin', 'Frankreich','LBS','USA','ZB_Freihand','ZB_UM_Lesesaal','Rara',
+    createModeFiles('defaultMode', ['RVK', 'NC', 'ZB_Magazin', 'Frankreich','LBS','USA','ZB_Freihand','ZB_UM_Lesesaal','Rara',
     'GFG_Film','GFG_Geographie','GFG_Geowissenschaften','GFG_Kunstgeschichte','GFG_Kunstgeschichte_Gr','GFG_Politikwissenschaft','GFG_Soziologie','GFG_Sport',
     'GFG_Zeile_quer_Erz','GFG_Zeile_quer_Geo','GFG_Zeile_quer_Psy','GFG_Zeile_quer_Pub','Kunsthochschule'])
   }
